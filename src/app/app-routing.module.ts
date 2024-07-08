ecf15dd8886ee66d09159d1383cb8ed49e083354
creativeyefr <contact@creative-eye.fr>
2024-07-08 22:22:15 +0200

    KR - Création de la page Restaurants List

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'restaurants',
+    loadChildren: () => import('./pages/restaurant-list/restaurant-list.module').then( m => m.RestaurantListPageModule)
   }
 ];
 
