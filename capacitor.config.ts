82eb41f8e26c26225b50054e877081b4321ec146
creativeyefr <contact@creative-eye.fr>
2024-08-03 10:47:58 +0200

    KR - Intégration de ESBuild

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'TravelChina',
-  webDir: 'www'
+  webDir: 'www/browser'
 };
 
 export default config;
